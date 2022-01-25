1f38e94b873609686341211127ad3082be889b17
mohraf <amosngok@gmail.com>
2022-01-25 11:27:12 +0300

    add user and repos constructors

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  access_token: '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'
 };
 
 /*
